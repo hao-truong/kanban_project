37739d58d49aa170a96c2283881a1be707d50e1b
hao-truong <hao-truong@cybozu.vn>
2023-12-13 16:29:37 +0700

    write unit test for some funtions in board service

--- back-end/src/test/BoardServiceTest.php
+++ back-end/src/test/BoardServiceTest.php
@@ -33,7 +33,7 @@ class BoardServiceTest extends TestCase
         $this->boardService = new BoardService($this->boardModelMock, $this->userServiceMock, $this->userBoardModelMock);
     }
 
-    public function testHandleCreateBoardSuccess(): void
+    public function testHandleCreateBoard(): void
     {
         $expect_result = self::$MATCHED_BOARD;
         $board_entity = new BoardEntity();
@@ -49,6 +49,161 @@ class BoardServiceTest extends TestCase
         ;
         $this->userBoardModelMock->expects($this->once())->method('save');
         $result = $this->boardService->handleCreateBoard($board_entity);
+        $this->assertEquals($expect_result, $result);
+    }
+
+    public function testHandleGetMyBoards(): void
+    {
+        $expect_result = [
+            [
+                'id'                => 67,
+                'title'             => 'Board 1',
+                'created_at'        => '2023-12-08 15:27:20.257',
+                'updated_at'        => '2023-12-08 15:27:20.257',
+                'creator_id'        => 25,
+                'number_of_members' => 3,
+            ],
+            [
+                'id'                => 68,
+                'title'             => 'Board 2',
+                'created_at'        => '2023-12-08 15:27:20.257',
+                'updated_at'        => '2023-12-08 15:27:20.257',
+                'creator_id'        => 26,
+                'number_of_members' => 2,
+            ]
+        ];
+        $this->userBoardModelMock
+            ->expects($this->once())
+            ->method('join')
+            ->with(
+                [
+                    'table'     => 'boards',
+                    'as'        => 'b',
+                    'condition' => [
+                        'board_id',
+                        'id'
+                    ],
+                    'select'    => [
+                        'id',
+                        'title',
+                        'created_at',
+                        'updated_at',
+                        'creator_id',
+                    ],
+                ],
+                [
+                    'where' => [
+                        'user_id',
+                        1,
+                    ]
+                ]
+            )
+            ->willReturn(
+                [
+                    [
+                        'id'         => 67,
+                        'title'      => 'Board 1',
+                        'created_at' => '2023-12-08 15:27:20.257',
+                        'updated_at' => '2023-12-08 15:27:20.257',
+                        'creator_id' => 25,
+                    ],
+                    [
+                        'id'         => 68,
+                        'title'      => 'Board 2',
+                        'created_at' => '2023-12-08 15:27:20.257',
+                        'updated_at' => '2023-12-08 15:27:20.257',
+                        'creator_id' => 26,
+                    ]
+                ]
+            )
+        ;
+        $matchers = $this->exactly(2);
+        $this->userBoardModelMock
+            ->expects($matchers)
+            ->method('count')
+            ->willReturnCallback(
+                function (string $field, int $board_id) use ($matchers) {
+                    match ([
+                        $field,
+                        $board_id
+                    ]) {
+                        [
+                            'board_id',
+                            67
+                        ] => 3,
+                        [
+                            'board_id',
+                            68
+                        ] => 2
+                    };
+                }
+            )
+            ->willReturn(3, 2)
+        ;
+        $result = $this->boardService->handleGetMyBoards(1);
+        $this->assertEquals($expect_result, $result);
+    }
+
+    public function testBoardsWithNumberOfMembers(): void
+    {
+        $expect_result = [
+            [
+                'id'                => 67,
+                'title'             => 'Board 1',
+                'created_at'        => '2023-12-08 15:27:20.257',
+                'updated_at'        => '2023-12-08 15:27:20.257',
+                'creator_id'        => 25,
+                'number_of_members' => 3,
+            ],
+            [
+                'id'                => 68,
+                'title'             => 'Board 2',
+                'created_at'        => '2023-12-08 15:27:20.257',
+                'updated_at'        => '2023-12-08 15:27:20.257',
+                'creator_id'        => 26,
+                'number_of_members' => 2,
+            ]
+        ];
+        $matchers = $this->exactly(2);
+        $this->userBoardModelMock
+            ->expects($matchers)
+            ->method('count')
+            ->willReturnCallback(
+                function (string $field, int $board_id) use ($matchers) {
+                    match ([
+                        $field,
+                        $board_id
+                    ]) {
+                        [
+                            'board_id',
+                            67
+                        ] => 3,
+                        [
+                            'board_id',
+                            68
+                        ] => 2
+                    };
+                }
+            )
+            ->willReturn(3, 2)
+        ;
+        $boards = [
+            [
+                'id'         => 67,
+                'title'      => 'Board 1',
+                'created_at' => '2023-12-08 15:27:20.257',
+                'updated_at' => '2023-12-08 15:27:20.257',
+                'creator_id' => 25,
+            ],
+            [
+                'id'         => 68,
+                'title'      => 'Board 2',
+                'created_at' => '2023-12-08 15:27:20.257',
+                'updated_at' => '2023-12-08 15:27:20.257',
+                'creator_id' => 26,
+            ]
+        ];
+        $result = $this->boardService->boardsWithNumberOfMembers($boards);
         $this->assertEquals($expect_result, $result);
     }
 }
